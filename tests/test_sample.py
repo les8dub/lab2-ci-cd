718dadb4b2eb1ec91e27481cdcc8b1b91d16b622
Lesya <lisadubkina@gmail.com>
2025-10-09 00:16:02 +0300

    test: restore simple test import

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,15 +1,4 @@
-import sys
-import os
-
-# Добавляем корень репозитория в sys.path, чтобы импорт main работал
-repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-if repo_root not in sys.path:
-    sys.path.insert(0, repo_root)
-
 from main import add
-
 
 def test_add():
     assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
